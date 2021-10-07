ec542b3d9f26764ce14d1c38c69f2f8b1108e010
Defi Factory Core Developer <defifactory@gmail.com>
2021-10-07 23:39:43 +0700

    no message

--- DeftLiquidityMigration.sol
+++ DeftLiquidityMigration.sol
@@ -14,23 +14,46 @@ contract DeftLiquidityMigration {
     
     address owner;
     
-    /* Eth */
+    /* Eth 
     address constant UNISWAP_V2_FACTORY_ADDRESS = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
     address constant UNISWAP_V2_ROUTER_ADDRESS = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
     address constant WETH_TOKEN_ADDRESS = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    address constant DEFT_TOKEN_ADDRESS = 0xdef1fac7Bf08f173D286BbBDcBeeADe695129840;
+    address constant DEFT_TOKEN_ADDRESS = 0xdef1fac7Bf08f173D286BbBDcBeeADe695129840;*/
     
-    /* Bsc
+    /* Bsc */
     address constant UNISWAP_V2_FACTORY_ADDRESS = 0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73;
     address constant UNISWAP_V2_ROUTER_ADDRESS = 0x10ED43C718714eb63d5aA57B78B54704E256024E;
     address constant WETH_TOKEN_ADDRESS = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
-    address constant DEFT_TOKEN_ADDRESS = 0xdef1fac7Bf08f173D286BbBDcBeeADe695129840;*/
+    address constant DEFT_TOKEN_ADDRESS = 0xdef1fac7Bf08f173D286BbBDcBeeADe695129840;
     
     /* Kovan 
     address constant UNISWAP_V2_FACTORY_ADDRESS = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
     address constant UNISWAP_V2_ROUTER_ADDRESS = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
     address constant WETH_TOKEN_ADDRESS = 0xd0A1E359811322d97991E03f863a0C30C2cF029C;
     address constant DEFT_TOKEN_ADDRESS = 0x7A7492a8e888Ca3fe8e31cB2E34872FE0CE5309f;*/
+    
+    /* Process:
+        - create uni v3 DEFT/USDC pair and add in DeftStorage
+        - create quick v2 DEFT/USDC pair and add in DeftStorage
+        - check if pancake DEFT/BUSD pair is created and added to DeftStorage
+        - pull liquidity uni v2 DEFT/ETH pair + screenshot
+        - pull liquidity pancake v2 DEFT/BNB pair + screenshot
+        - deposit 100% ETH to binance.com + screenshot
+        - deposit 100% BNB to binance.com + screenshot
+        - convert BNB to ETH + screenshot
+        - convert 50% ETH to BUSD + screenshot
+        - convert 40% ETH to Matic + screenshot
+        - convert 10% ETH to USDC + screenshot
+        - withdraw BUSD to BSC + screenshot
+        - withdraw Matic to Polygon + screenshot
+        - withdraw USDC to ETH + screenshot
+        - swap Matic for USDC on quick v2 + screenshot
+        - add liquidity on uni v3 DEFT/USDC pair + screenshot
+        - add liquidity on quick v2 DEFT/USDC pair + screenshot
+        - balance price on pancake v2 DEFT/BUSD pair + screenshot
+        - add liquidity on pancake v2 DEFT/BUSD pair + screenshot
+    */
+    
     
     constructor() {
         owner = msg.sender;
